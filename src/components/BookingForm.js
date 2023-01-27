9f028d34ab1c7572e271a4de2c6d6f4abf521f31
ShahzaibShoukat <shahzaib_shoukat@yahoo.com>
2023-01-27 17:31:42 +0500

    Booking form is ready with form field validation

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,16 +1,25 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom"
 
+
+const PasswordErrorMessage = () => {
+    return (
+      <p className="FieldError">Password should have at least 8 characters</p>
+    );
+};
+
+
+
 const BookingForm = (props) => {
     const [date, setDate] = useState("");
-    const [time, setTime] = useState("17:00");
-    const [guests, setGuests] = useState("");
-    const [occasion, setOccasion] = useState("");
+    const [time, setTime] = useState("00:00");
+    const [guests, setGuests] = useState("0");
+    const [occasion, setOccasion] = useState("occasion");
     const clearForm = () => {
         setDate("");
-        setTime("17:00");
+        setTime("00:00");
         setGuests("0");
-        setOccasion("");
+        setOccasion("occasion");
     };
 
     let bookingState1 = props.bookingState;
@@ -19,30 +28,41 @@ const BookingForm = (props) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if(bookingState1 == true) {onSuccess('/booking-confirmation')};
+        if(bookingState1 === true) {onSuccess('/booking-confirmation')};
         clearForm();
     };
 
+    const getIsFormValid = () => {
+        return (
+          date &&
+          time !== "select-time" &&
+          guests > 0 &&
+          occasion !== "select-occasion"
+        );
+    };
+
     return (
-        <div className="reservation text-center">
-            <h1>Available time<br></br>{props.availableTimes.join(" | ")}</h1>
+        <div className="reservation">
+            <h1 className="text-center">Available time<br></br>{props.availableTimes.join(" | ")}</h1>
             <form onSubmit={handleSubmit}>
-                <label htmlFor="res-date">Choose date</label>
+                <label htmlFor="res-date">Choose date<strong className="star">*</strong></label>
                 <input value={date} onChange={(e) => {setDate(e.target.value);}}
-                    type="date" id="res-date" />
-                <label htmlFor="res-time">Choose time</label>
+                    type="date" id="res-date" aria-label="Choose date" />
+                <label htmlFor="res-time" aria-label="Choose time" >Choose time<strong className="star">*</strong></label>
                 <select id="res-time" value={time} onChange={(e) => {setTime(e.target.value);}}>
-                    {props.availableTimes.map((time) => <option>{time}</option>)}
+                <option value={"select-time"}>Select Time</option>
+                    {props.availableTimes.map((time) => <option value={time}>{time}</option>)}
                 </select>
-                <label htmlFor="guests">Number of guests</label>
+                <label htmlFor="guests">Number of guests<strong className="star">*</strong></label>
                 <input value={guests} onChange={(e) => {setGuests(e.target.value);}}
-                    type="number" placeholder="1" min="1" max="10" id="guests" />
-                <label htmlFor="occasion">Occasion</label>
+                    type="number" placeholder="1" min="0" max="20" id="guests" aria-label="Number of guests" />
+                <label htmlFor="occasion" aria-label="Occasion">Occasion<strong className="star">*</strong></label>
                 <select id="occasion" value={occasion} onChange={(e) => {setOccasion(e.target.value);}}>
-                    <option>Birthday</option>
-                    <option>Anniversary</option>
+                    <option value="select-occasion">Select Occasion</option>
+                    <option value="birthday">Birthday</option>
+                    <option value="anniversary">Anniversary</option>
                 </select>
-                <button type="submit">
+                <button disabled={!getIsFormValid()} type="submit">
                     Make Your reservation
                 </button>
             </form>
--- src/pages/Reservation.js
+++ src/pages/Reservation.js
@@ -3,7 +3,7 @@ import BookingForm from "../components/BookingForm";
 const Reservation = (props) => {
   return (
     <>
-      <h1 className="text-center">Reservation</h1>
+      <h1 className="text-center">Reservation form</h1>
       <div className="center">
         <BookingForm  availableTimes={props.availableTimes} bookingState={props.bookingState} />
       </div>
