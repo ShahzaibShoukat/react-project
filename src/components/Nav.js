9dd4fc31f0f37669e1f5f796be0fc97a81e84219
ShahzaibShoukat <shahzaib_shoukat@yahoo.com>
2023-03-08 04:18:44 +0500

    Submit project

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,42 +1,45 @@
 import { Link } from "react-router-dom";
+import React, { useEffect } from 'react';
 
 const Nav = () => {
-    let menuButton = () => {
-        var x = document.getElementById("myTopnav");
-        if (x.className === "topnav") {
-          x.className += " responsive";
-        } else {
-          x.className = "topnav";
+  let menuButton = () => {
+      var x = document.getElementById("myTopnav");
+      if (x.className === "topnav") {
+        x.className += " responsive";
+      } else {
+        x.className = "topnav";
+      }
+    }
+  useEffect(() => {
+    const navLinks = document.querySelectorAll(".topnav a");
+    for (let i = 0; i < navLinks.length; i++) {
+      navLinks[i].addEventListener("click", function() {
+        for (let j = 0; j < navLinks.length; j++) {
+          navLinks[j].classList.remove("active");
         }
-      }
-      const navLinks = document.querySelectorAll(".topnav a");
-      for (let i = 0; i < navLinks.length; i++) {
-        navLinks[i].addEventListener("click", function() {
-          for (let j = 0; j < navLinks.length; j++) {
-            navLinks[j].classList.remove("active");
-          }
-          this.classList.add("active");
-        });
-      }
-    return (
-        <nav>
-          <div className="container d-flex jc-space-between">
-            <img src="/logo.svg" className="ms-1" alt="Little lemon"></img>
-            <div className="nav-bar">
-              <div className="topnav" id="myTopnav">
-                <Link className="active" to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/menu">Menu</Link>
-                <Link to="/reservation">Reservation</Link>
-                <Link to="/order-online">Order Online</Link>
-                <Link to="/login">Login</Link>
-                <a className="icon" onClick={menuButton}>
-                    <i className="fa fa-bars"></i>
-                </a>
-              </div>
+        this.classList.add("active");
+      });
+    }
+  }, []);
+  return (
+      <nav>
+        <div className="container d-flex jc-space-between">
+          <img src="/logo.svg" className="ms-1" alt="Little lemon"></img>
+          <div className="nav-bar">
+            <div className="topnav" id="myTopnav">
+              <Link className="active" to="/">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/menu">Menu</Link>
+              <Link to="/reservation">Reservation</Link>
+              <Link to="/order-online">Order Online</Link>
+              <Link to="/login">Login</Link>
+              <a href="#" className="icon" onClick={menuButton}>
+                  <i className="fa fa-bars"></i>
+              </a>
             </div>
           </div>
-        </nav>
+        </div>
+      </nav>
     );
   };
 
